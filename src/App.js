cd4c087283470d3a0b7c882e4602179682df56f5
VasilikiPapageorgiou <vasil.papage@gmail.com>
2020-07-29 14:12:30 +0300

    correct the README file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> kalispera friends.
+          Edit <code>src/App.js</code> kalispera friends. yyyy
         </p>
         <a
           className="App-link"
