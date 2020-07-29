3f114a99c261ad108e34ea069d0685e7dc6f1bd3
VasilikiPapageorgiou <vasil.papage@gmail.com>
2020-07-29 13:03:45 +0300

    dummy commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and sadjfhldjflksjfoad.
+          Edit <code>src/App.js</code> kalispera friends.
         </p>
         <a
           className="App-link"
