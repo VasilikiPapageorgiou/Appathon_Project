f4e76b47c54c003ed39be18a141f1015d298040e
VasilikiPapageorgiou <vasil.papage@gmail.com>
2020-07-30 15:13:22 +0300

    we reach the database

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -4,11 +4,19 @@ const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient 
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'air-polltion'
+const databaseName = 'test'
 
 MongoClient.connect(connectionURL, { useNewParser: true }, (error, client) => { 
     if (error) {
         return console.log('Unable to connect to database')
     }
-    console.log('Connected correctly!')
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'ChristiMaria',
+        age: 21
+    })
+    console.log('hiiii')
+
 })
